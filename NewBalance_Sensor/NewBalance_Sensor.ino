856a9048d55fcd024f19118b07216051f168a534
jhYun505 <y_jh0505@naver.com>
2023-02-11 16:04:38 -0500

    Add code for sending data to ThingsBoard

--- NewBalance_Sensor/NewBalance_Sensor.ino
+++ NewBalance_Sensor/NewBalance_Sensor.ino
@@ -1,6 +1,10 @@
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <ThingsBoard.h>
+#include <ArduinoJson.h>
 
 
 #define FORCE_SENSOR_PIN1 36 // ESP32 pin GIOP36 (ADC0): the FSR and 10K pulldown are connected to A0
@@ -11,32 +15,43 @@
 
 // Baud rate for the debugging serial connection
 constexpr uint32_t SERIAL_DEBUG_BAUD PROGMEM = 115200U;
-
-
-
-
-
+// WiFi information
+constexpr char WIFI_SSID[] PROGMEM = "YOUR_WIFI_NAME";
+constexpr char WIFI_PASSWORD[] PROGMEM = "YOUR_WIFI_PASSWORD";
+// See https://thingsboard.io/docs/getting-started-guides/helloworld/
+// to understand how to obtain an access token
+constexpr char TOKEN[] PROGMEM = "YOUR_THIGNSBOARD_TOKEN";
+// Thingsboard we want to establish a connection too
+constexpr char THINGSBOARD_SERVER[] PROGMEM = "demo.thingsboard.io";
+// MQTT port used to communicate with the server, 1883 is the default unencrypted MQTT port
+constexpr uint16_t THINGSBOARD_PORT PROGMEM = 1883U;
+// Maximum size packets will ever be sent or received by the underlying MQTT client,
+// if the size is to small messages might not be sent or received messages will be discarded
+constexpr uint32_t MAX_MESSAGE_SIZE PROGMEM = 256U;   // Increased 128 -> 256
+// Initialize underlying client, used to establish a connection
+WiFiClient espClient;
+// Initialize ThingsBoard instance with the maximum needed buffer size
+ThingsBoardSized<MAX_MESSAGE_SIZE> tb(espClient);
 //create mpu object
 Adafruit_MPU6050 mpu;
 // create variables for save FSR signal
-int analogReading1, analogReading2, analogReading3, analogReading4;
+int fsrReading1, fsrReading2, fsrReading3, fsrReading4;
+// Change when Start/Stop Button is clicked
+bool SendingData = true;
 
-void setup() {
-  // Initialize serial connection for debugging
-  Serial.begin(SERIAL_DEBUG_BAUD);
+void InitMPU() {
   Wire.begin();
   while(!Serial){
     delay(100);
   }
 
   if(!mpu.begin()) {
-    // Print error message
     Serial.println("Failed to find mpu chip");
     while(1) {
       delay(100);
     }
   }
-  
+
   // Setting MPU start
   // The measurement range of accelerometer : 16g
   mpu.setAccelerometerRange(MPU6050_RANGE_16_G);
@@ -47,33 +62,92 @@ void setup() {
   // Setting MPU end
   Serial.println("");
   delay(100);
+}
 
+void InitWiFi() {
+  Serial.println("Connecting to AP ...");
+  // Attempting to establish a connection to the given WiFi network
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    // Delay 500ms until a connection has been succesfully established
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to AP");
+}
+
+/// @brief Reconnects the WiFi uses InitWiFi if the connection has been removed
+/// @return Returns true as soon as a connection has been established again
+const bool reconnect() {
+  // Check to ensure we aren't connected yet
+  const wl_status_t status = WiFi.status();
+  if (status == WL_CONNECTED) {
+    return true;
+  }
+  // If we aren't establish a new connection to the given WiFi network
+  InitWiFi();
+  return true;
+}
+
+void setup() {
+  // If analog input pin 0 is unconnected, random analog
+  // noise will cause the call to randomSeed() to generate
+  // different seed numbers each time the sketch runs.
+  // randomSeed() will then shuffle the random function.
+  randomSeed(analogRead(0));
+  // Initialize serial connection for debugging
+  Serial.begin(SERIAL_DEBUG_BAUD);
+  delay(1000);
+  InitWiFi();
+  InitMPU();
 }
 
 void loop() {
+  if(WiFi.status() != WL_CONNECTED){
+    reconnect();
+  }
+
+  if(!tb.connected()) {
+    Serial.print("Connecting to: ");
+    Serial.print(THINGSBOARD_SERVER);
+    Serial.print(" with token ");
+    Serial.println(TOKEN);
+    if(!tb.connect(THINGSBOARD_SERVER, TOKEN, THINGSBOARD_PORT)) {
+      Serial.println("Failed to connect");
+      return;
+    }
+  }
+
+  if(SendingData){
+    sendData();
+  }
+
+}
+
+void sendData(){
   sensors_event_t a, g, temp;    //instances to read sensor value
+  // Reading Start
   mpu.getEvent(&a, &g, &temp);
-  analogReading1 = analogRead(FORCE_SENSOR_PIN1);
-  analogReading2 = analogRead(FORCE_SENSOR_PIN2);
-  analogReading3 = analogRead(FORCE_SENSOR_PIN3);
-  analogReading4 = analogRead(FORCE_SENSOR_PIN4); 
+  fsrReading1 = analogRead(FORCE_SENSOR_PIN1);
+  fsrReading2 = analogRead(FORCE_SENSOR_PIN2);
+  fsrReading3 = analogRead(FORCE_SENSOR_PIN3);
+  fsrReading4 = analogRead(FORCE_SENSOR_PIN4); 
 
-  // Print of MPU
-  Serial.print("MPU value [");
-  // Adafruit lib
-  Serial.print("AccelX: "); Serial.print(a.acceleration.x); Serial.print(", ");
-  Serial.print("AccelY: "); Serial.print(a.acceleration.y); Serial.print(", ");
-  Serial.print("AccelZ: "); Serial.print(a.acceleration.z); Serial.print(", ");
-  Serial.print("GyroX: "); Serial.print(g.gyro.x); Serial.print(", ");
-  Serial.print("GyroY: "); Serial.print(g.gyro.y); Serial.print(", ");
-  Serial.print("GyroZ: "); Serial.print(g.gyro.z); Serial.print("]\t");
-  // Print of FSR
-  Serial.print("FSR value ["); 
-  Serial.print("1st FSR: "); Serial.print(analogReading1); Serial.print(", ");
-  Serial.print("2nd FSR: "); Serial.print(analogReading2); Serial.print(", ");
-  Serial.print("3rd FSR: "); Serial.print(analogReading3); Serial.print(", ");
-  Serial.print("4th FSR: "); Serial.print(analogReading4); Serial.println("]");
+  StaticJsonDocument<500> SensorValues;
+  SensorValues["AccelX"] = a.acceleration.x;
+  SensorValues["AccelY"] = a.acceleration.y;
+  SensorValues["AccelZ"] = a.acceleration.z;
+  SensorValues["GyroX"] = g.gyro.x;
+  SensorValues["GyroY"] = g.gyro.y;
+  SensorValues["GyroZ"] = g.gyro.z;
+  SensorValues["1st FSR"] = fsrReading1;
+  SensorValues["2nd FSR"] = fsrReading2;
+  SensorValues["3rd FSR"] = fsrReading3;
+  SensorValues["4th FSR"] = fsrReading4;
 
+  char buffer[500];
+  serializeJson(SensorValues, buffer);
+  tb.sendTelemetryJson(buffer);
+  Serial.println("Data Sending...");
   delay(100);
-
 }
